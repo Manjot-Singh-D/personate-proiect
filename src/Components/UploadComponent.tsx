d4e06c3d33db95bd95d80f657745c793ad2bdc4f
manjot-singh-d <manjotsinghdhanjal@gmail.com>
2023-03-25 15:24:35 +0530

    ShowVideo Done

--- src/Components/UploadComponent.tsx
+++ src/Components/UploadComponent.tsx
@@ -6,11 +6,15 @@ import { S3Client } from "@aws-sdk/client-s3";
 const UploadComponent: React.FC = () => {
     const [selectedFile, setSelectedFile] = useState();
     const [progress, setProgress] = useState(0);
+    const [showVideo, setShowVideo] = useState<string>();
     const handleFileInput = (e: any): void => {
+        console.log(e.target.files);
         setSelectedFile(e.target.files[0]);
+        setProgress(0);
     }
     const uploadFile = (): void => {
         if (selectedFile) {
+            console.log(process.env)
             const target = { Bucket: `${process.env.REACT_APP_AWS_BUCKET_NAME}`, Key: selectedFile["name"], Body: selectedFile };
             const region = `${process.env.REACT_APP_REGION}`;
             const creds = { accessKeyId: `${process.env.REACT_APP_ACCESS_KEY_ID}`, secretAccessKey: `${process.env.REACT_APP_SECRET_ACCESS_KEY}` };
@@ -20,12 +24,18 @@ const UploadComponent: React.FC = () => {
                     leavePartsOnError: false,
                     params: target,
                 })
-
+                console.log(parallelUploads3);
                 parallelUploads3.on("httpUploadProgress", (prog) => {
+                    console.log(prog);
+                    console.log(selectedFile)
                     if (prog && prog?.total && prog?.loaded) {
                         const perc = Math.round((prog.loaded / prog.total) * 100);
                         if (perc === 100) {
+
+                            const url = `${process.env.REACT_APP_OBJECT_URL}${selectedFile["name"]}`
+                            setShowVideo(url);
                             console.log("COMPLETED !!!");
+
                         }
                         setProgress(perc);
                     }
@@ -38,6 +48,10 @@ const UploadComponent: React.FC = () => {
     }
     return (
         <div>
+            {showVideo &&
+                <video width="450" height="300" controls key={showVideo} >
+                    <source src={showVideo} type="video/mp4" />
+                </video>}
             <div>File Upload Progress is {progress}%</div>
             <input type="file" onChange={handleFileInput} />
             <button onClick={uploadFile}> Upload to S3</button>
